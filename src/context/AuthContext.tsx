9b07a718362aa5fd4c034f816df63a940330dfbd
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-11-17 20:44:38 -0300

    First Commit

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode, useContext } from "react";
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+    signIn(credentials: SignInCredentials): Promise<void>;
+    isAuthenticated: boolean;
+}
+
+type AuthProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextData)
+
+export function AuthProvider ({ children }: AuthProviderProps){
+  const isAuthenticated = false;
+
+  const signIn = async ({ email, password }: SignInCredentials) => {
+    console.log({ email, password})
+  }
+
+  return(
+    <AuthContext.Provider value={{ signIn, isAuthenticated}}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
+
+export function useAuth() {
+   const context = useContext(AuthContext);
+   return context;
+}
