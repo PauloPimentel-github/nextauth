79218ea59b629d591f23016f0af14eef31571c77
PauloPimentel-github <paulo.h.g.pimentel@gmail.com>
2022-11-28 20:40:50 -0300

    Autenticação e autorização - Criando componente de permissão

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../context/AuthContext";
-import { useCan } from "../hooks/useCan";
 
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
@@ -8,11 +8,6 @@ import { withSSRGAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
     const { user } = useContext(AuthContext)
-
-    const userCanSeeMetrics = useCan({
-        //permissions: ['metrics.list']
-        roles: ['administrator', 'editor']
-    })
 
     useEffect(() => {
         api.get('/me')
@@ -24,7 +19,9 @@ export default function Dashboard() {
         <>
             <h1>Dashboard: { user?.email }</h1>
 
-            { userCanSeeMetrics && <div>Métricas</div> }
+            <Can permissions={['metrics.list']}>
+                <div>Métricas</div> 
+            </Can>
         </>
     )
 }
